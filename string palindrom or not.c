1820f4e92fce4eca7852a87dee77963f012cf9d9
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2018-12-06 12:58:00 +0530

    Create string palindrom or not.c

---
+++ string palindrom or not.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    char s[100],s1[100];
+    int i,j=0,l=0,c=0;
+    scanf("%s",s);
+    while(s[j]!='\0')
+    j++;
+    for(i=j-1;i>=0;i--)
+    {
+     s1[l]=s[i];
+     l++;
+    }
+    for(i=0;i<j;i++)
+    {
+        if(s1[i]==s[i])
+        c=c+1;
+    }
+    if(c==j)
+    printf("yes");
+    else
+    printf("no");
+    
+    
+
+    return 0;
+}
