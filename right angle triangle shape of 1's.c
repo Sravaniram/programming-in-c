775d25890efb2fab9c20f0862b4d36838c5e123f
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2018-12-11 12:11:53 +0530

    Create right angle triangle shape of 1's.c

---
+++ right angle triangle shape of 1's.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=n;i>0;i--)
+    {
+        for(j=i;j>0;j--){
+        printf("1");}
+        printf("\n");
+    }
+
+    return 0;
+}
