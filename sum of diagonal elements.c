4647b975928b4b3d1fe19d680031adae4972141c
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2018-12-14 06:43:13 +0530

    Create sum of diagonal elements.c

---
+++ sum of diagonal elements.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,k,n,a[10][10];
+    n=3;
+    k=0;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    
+     for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(i==j)
+            k=k+a[i][j];
+           
+            
+        }
+    }
+    printf("%d",k);
+return 0;
+    
+}
