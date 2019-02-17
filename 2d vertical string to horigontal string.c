a044c46260b30ed567861b5f01a10c46276d5807
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2019-02-17 14:56:18 +0530

    Update 2d vertical string to horigontal string.c

--- 2d vertical string to horigontal string.c
+++ 2d vertical string to horigontal string.c
@@ -1,10 +1,9 @@
-
 #include <stdio.h>
 int main()
 {char a[100],b[100];
 int i=0;
-scanf("%s",a);
-scanf("%s",b);
+scanf("%[^\n]%*c",a);
+scanf("%[^\n]%*c",b);
 while(a[i]!='\0' || b[i]!='\0')
 {if(i==0)
 printf("%c%c",b[i],a[i]);
